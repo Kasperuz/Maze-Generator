beba447a2c0fd7e596c5b9777acfec77059e11a6
Kasper Bergman <kasperbergman313@gmail.com>
2023-03-16 19:26:04 +0100

    Changed language

--- assets/game world/World generator/World Generator2.gd
+++ assets/game world/World generator/World Generator2.gd
@@ -1,77 +1,76 @@
 extends Node
 
 
-@export var Storlek: Vector2
-@export_range(0,1) var SvängChans: float
-@export var tileMap: TileMap
+@export var Size: Vector2
+@export_range(0,1) var TurnChance: float
+@export var OutputTilemap: TileMap
 
-var krysset := [Vector2i(0,-1),Vector2i(1,0),Vector2i(0,1),Vector2i(-1,0)]
-var hörnen := [Vector2i(1,1),Vector2i(-1,1),Vector2i(1,-1),Vector2i(-1,-1)]
-
-var indexIMapHistory := 0
+var cross := [Vector2i(0,-1),Vector2i(1,0),Vector2i(0,1),Vector2i(-1,0)]
+var corners := [Vector2i(1,1),Vector2i(-1,1),Vector2i(1,-1),Vector2i(-1,-1)]
 
 var rng := RandomNumberGenerator.new()
 
 
-func ritaKarta(karta):
-	for x in range(len(karta)):
-		for y in range(len(karta[x])):
-			tileMap.set_cell(0, Vector2i(x, y), 0, Vector2i(0,karta[x][y]))
+func drawMapToTilemap(map):
+	for x in range(len(map)):
+		for y in range(len(map[x])):
+			OutputTilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(0,map[x][y]))
 
-func checka(map:Array, position:Vector2i) -> bool:
+func check(map:Array, position:Vector2i) -> bool:
 	return map[position.x][position.y] == 3
 
-func blandaLista(lista:Array):
-	for i in range(len(lista)):
+func mixList(list:Array):
+	for i in range(len(list)):
 		var index1 = i
-		var index2 = rng.randi_range(0,len(lista)-1)
-		var t = lista[index1]
-		lista[index1] = lista[index2]
-		lista[index2] = t
+		var index2 = rng.randi_range(0,len(list)-1)
+		var t = list[index1]
+		list[index1] = list[index2]
+		list[index2] = t
 
-func sättPixel(map:Array, position:Vector2, värde:int):
-	map[position.x][position.y] = värde
+func setPixel(map:Array, position:Vector2, value:int):
+	map[position.x][position.y] = value
 
-func ritaUtPixel(map:Array, position:Vector2i, riktning:int, värde:int):
-	map[position.x][position.y] = värde
-	var positionB = position + krysset[riktning]
+func drawPixel(map:Array, position:Vector2i, direction:int, value:int):
+	map[position.x][position.y] = value
+	var positionB = position + cross[direction]
 	var mapPositionB = map[positionB.x][positionB.y]
 		
 	if mapPositionB == 0:
-		sättPixel(map, positionB, 3)
+		setPixel(map, positionB, 3)
 	elif mapPositionB == 2 or mapPositionB == 3:
-		sättPixel(map, positionB, 4)
-	var krC = Vector2i(krysset[riktning].y, krysset[riktning].x)
+		setPixel(map, positionB, 4)
+	var krC = Vector2i(cross[direction].y, cross[direction].x)
 	var positionC = position + krC
 	var mapPositionC = map[positionC.x][positionC.y]		
 	if mapPositionC == 2:
-		sättPixel(map, positionC, 3)
+		setPixel(map, positionC, 3)
 	elif mapPositionC == 3:
-		sättPixel(map, positionC, 4)
+		setPixel(map, positionC, 4)
 
 	positionC = position - krC
 	mapPositionC = map[positionC.x][positionC.y]
 	if mapPositionC == 2:
-		sättPixel(map, positionC, 3)
+		setPixel(map, positionC, 3)
 	elif mapPositionC == 3:
-		sättPixel(map, positionC, 4)
+		setPixel(map, positionC, 4)
 
-	positionC = position + krC + krysset[riktning]
+	positionC = position + krC + cross[direction]
 	mapPositionC = map[positionC.x][positionC.y]
 	if mapPositionC == 0:
-		sättPixel(map, positionC, 2)
+		setPixel(map, positionC, 2)
 	elif mapPositionC == 2 or mapPositionC == 3:
-		sättPixel(map, positionC, 4)
+		setPixel(map, positionC, 4)
 
-	positionC = position - krC + krysset[riktning]
+	positionC = position - krC + cross[direction]
 	mapPositionC = map[positionC.x][positionC.y]
 	if mapPositionC == 0:
-		sättPixel(map, positionC, 2)
+		setPixel(map, positionC, 2)
 	elif mapPositionC == 2 or mapPositionC == 3:
-		sättPixel(map, positionC, 4)
+		setPixel(map, positionC, 4)
 
 
 func generate(storlek:Vector2i, startPosition:Vector2i,) -> Array:
+	rng.randomize()
 	var map := []
 	for x in range(storlek.x):
 		map.append([])
@@ -82,73 +81,71 @@ func generate(storlek:Vector2i, startPosition:Vector2i,) -> Array:
 				map[x].append(0)
 			
 			
-	var riktning = 0
+	var direction = 0
 	
 	var position := startPosition
 	map[position.x][position.y] = 1
-	for i in krysset:
-		sättPixel(map, position + i, 3)
-	for i in hörnen:
-		sättPixel(map, position + i, 2)
+	for i in cross:
+		setPixel(map, position + i, 3)
+	for i in corners:
+		setPixel(map, position + i, 2)
 
-#	for aa in range(3):
 	while true:
 		while true:
-			if rng.randf_range(0.0,1.0) < SvängChans:
-				#Om man ska byta riktning
-				blandaLista(krysset)
-				var hittade := false
-				for riktningAttTesta in range(len(krysset)):
-					if checka(map, position + krysset[riktningAttTesta]):
-						riktning = riktningAttTesta
-						hittade = true
+			if rng.randf_range(0.0,1.0) < TurnChance:
+				#Om man ska byta direction
+				mixList(cross)
+				var found := false
+				for directionToTest in range(len(cross)):
+					if check(map, position + cross[directionToTest]):
+						direction = directionToTest
+						found = true
 						break
-				if !hittade:
+				if !found:
 					break
 			else:
-				if !checka(map,position+krysset[riktning]):
-					#Om man ska byta riktning
-					blandaLista(krysset)
-					var hittade := false
-					for riktningAttTesta in range(len(krysset)):
-						if checka(map, position + krysset[riktningAttTesta]):
-							riktning = riktningAttTesta
-							hittade = true
+				if !check(map,position+cross[direction]):
+					#Om man ska byta direction
+					mixList(cross)
+					var found := false
+					for directionToTest in range(len(cross)):
+						if check(map, position + cross[directionToTest]):
+							direction = directionToTest
+							found = true
 							break
-					if !hittade:
+					if !found:
 						break
-			position += krysset[riktning]
-			ritaUtPixel(map, position, riktning, 1)
-			ritaKarta(map)
+			position += cross[direction]
+			drawPixel(map, position, direction, 1)
+			drawMapToTilemap(map)
 
-		var hittade = false
-		var grenKandidater := []
+		var found := false
+		var branchCandidates := []
 		for x in range(len(map)):
 			for y in range(len(map[x])):
 				if map[x][y] == 3:
-					grenKandidater.append(Vector2i(x,y))
+					branchCandidates.append(Vector2i(x,y))
 					
-		if len(grenKandidater) <= 0:
+		if len(branchCandidates) <= 0:
 			return map
 			
-		position = grenKandidater[rng.randi_range(0, len(grenKandidater) - 1)]
-		for i in range(len(krysset)):
-			if map[krysset[i].x + position.x][krysset[i].y + position.y] == 1:
-				riktning = i
-				hittade = true
+		position = branchCandidates[rng.randi_range(0, len(branchCandidates) - 1)]
+		for i in range(len(cross)):
+			if map[cross[i].x + position.x][cross[i].y + position.y] == 1:
+				direction = i
+				found = true
 				break
-		if !hittade:
+		if !found:
 			print("error")
 			breakpoint
-		for i in range(len(krysset)):
-			if krysset[i].x + krysset[riktning].x == 0 && krysset[i].y + krysset[riktning].y == 0:
-				riktning = i
+		for i in range(len(cross)):
+			if cross[i].x + cross[direction].x == 0 && cross[i].y + cross[direction].y == 0:
+				direction = i
 				break
 			
-		ritaUtPixel(map,position,riktning,1)
-		ritaKarta(map)
+		drawPixel(map,position,direction,1)
+		drawMapToTilemap(map)
 	return map
 
 func _ready():
-	rng.randomize()
-	ritaKarta(generate(Storlek,Vector2i(5,5)))
+	drawMapToTilemap(generate(Size,Vector2i(5,5)))
